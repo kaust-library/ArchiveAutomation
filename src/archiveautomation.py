a258460bbb571512a28d47f6d2e39a27ba4c7770
Marcelo Garcia <marcelomgarcia@gmail.com>
2021-11-08 12:13:42 +0300

    Created a bag with hard coded values.

--- src/archiveautomation.py
+++ src/archiveautomation.py
@@ -6,9 +6,22 @@
 import os
 import sys
 import requests
+import bagit
+import pathlib
+from datetime import datetime
 
-def archivera_to_bagit(BagIt_to_Archivera, my_accession):
+def archivera_to_bagit(BagIt_to_Archivera, my_accession, bag_path):
     """Convert an accession from Archivera to a bag-info file"""
+
+    # Create a simple BagIt file, and initialize the bag info with creation of the bag.
+    my_bag = bagit.make_bag(bag_path, {'Bagging-Date': datetime.now().isoformat()})
+
+    # Update BagIt metadata
+    for kk in BagIt_to_Archivera.keys():
+        my_bag.info[BagIt_to_Archivera[kk]] = my_accession['records'][0][kk][0]['display']
+    my_bag.save()
+
+    return ""
 
 def get_accession(my_api_conf, my_headers, dt_acc):
     """Return an accession from a query with ArchivEra API."""
@@ -106,26 +119,27 @@ if __name__ == "__main__":
 
     # Create a dictionay bagIt to Archivera
     BagIt_to_Archivera = {
-        'Source-Organization': 'AU.AUCr.Term',
-        'External-Identifier': 'ACCXAN',
-        'Internal-Sender-Description': 'ACCDES',
-        'Title': 'TI',
-        'Date-Start': 'ACCTIMPD',
-        'Record-Creators': 'ACCBYP.BYPA.NAMESTRANS',
-        'Record-Type': 'RTYPE.CodeDesc'
-    }
-    
+        'AU.AUCr.Term': 'Source-Organization',
+        'ACCXAN': 'External-Identifier',
+        'ACCDES': 'Internal-Sender-Description',
+        'TI': 'Title',
+        'ACCTIMPD': 'Date-Start',
+        'ACCBYP.BYPA.NAMESTRANS':'Record-Creators',
+		'RTYPE.CodeDesc': 'Record-Type'
+	}
+
     # Read accession by accession number (ACCXAN)
     acc_number = '013_001_0003'
     dt_acc = {}
     dt_acc['command'] = f"ACCXAN=='{acc_number}'"
-    dt_acc['fields'] = ",".join([vv for vv in BagIt_to_Archivera.values()])
+    dt_acc['fields'] = ",".join([kk for kk in BagIt_to_Archivera.keys()])
 
     my_accession = get_accession(my_api_conf, my_headers, dt_acc)
 
-    print(my_accession)
-    
-    my_bag = archivera_to_bagit(BagIt_to_Archivera, my_accession)
+    #print(my_accession)
 
     # Create BagIt file
+    bag_path = pathlib.Path(r'C:\Users\garcm0b\OneDrive - KAUST\Documents\Work\my_bag1')
+    my_bag = archivera_to_bagit(BagIt_to_Archivera, my_accession, bag_path)
+
 
