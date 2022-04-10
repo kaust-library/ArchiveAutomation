7210bdcbe112e5efd2bbead69701694e40fb79c0
Marcelo Garcia <marcelomgarcia@gmail.com>
2022-04-10 15:39:54 +0300

    testing split again

--- aalib.py
+++ aalib.py
@@ -31,7 +31,7 @@ def droid_run(droid_config, bag_path, acc_number):
         droid_cmd = pathlib.Path(f"{droid_exec_path} -a {droid_bag_path} -p {acc_number}.droid")
         print(f"Creating droid profile...")
         print(f"Running droid command {droid_cmd}")
-        result = subprocess.run(droid_cmd,  stdout=subprocess.PIPE, 
+        result = subprocess.run(droid_cmd.split(),  stdout=subprocess.PIPE, 
             stderr=subprocess.STDOUT, text=True)
         result.check_returncode()
         print(results.stdout)
