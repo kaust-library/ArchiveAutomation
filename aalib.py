98c5d00658bbf6bc855cfab9ac2f1ccfe37869c6
Marcelo Garcia <marcelo.garcia@kaust.edu.sa>
2022-06-09 16:06:57 +0300

    update

--- aalib.py
+++ aalib.py
@@ -205,7 +205,7 @@ def av_run(av_config):
 
     av_log_file = f"{av_config['av_logs_root']}_{av_config['av_accession']}_{av_run_date}.txt"
     clamav_bin_file = pathlib.Path( av_config['av_dir'], av_config['av_clamav'] )
-    av_check = f" {clamav_bin_file} --recursive \"{av_config['av_location']}\" -v -a -l {av_log_file}"
+    av_check = f" {clamav_bin_file} --recursive \'{av_config['av_location']}\' -v -a -l {av_log_file}"
     print(f"Antivirus check: {av_check}", end='... ')
     result = subprocess.run(av_check, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
     print("done.")
