70903ac16c71b6a9f3fd59e3ce672836cc403f1e
Marcelo Garcia <marcelo.garcia@kaust.edu.sa>
2022-06-09 17:07:24 +0300

    typo

--- aalib.py
+++ aalib.py
@@ -207,7 +207,7 @@ def av_run(av_config):
     clamav_bin_file = pathlib.Path( av_config['av_dir'], av_config['av_clamav'] )
     av_check = f" {clamav_bin_file} --recursive \"{av_config['av_location']}\" -v -a -l \"{av_log_file}\""
     print(f"Antivirus check: {av_check}", end='... ')
-    result = subprocess.run(av_cleck, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    result = subprocess.run(av_check, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
     print("done.")
 
     print(f"clamav log: {result.stdout}")
